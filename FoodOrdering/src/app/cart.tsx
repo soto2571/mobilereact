20b7b017dded33e622467e69b3f05d0b0178e0c4
Sebastian <sebastian.soto1649@gmail.com>
2025-01-11 19:11:30 -0400

    Auto commit

--- FoodOrdering/src/app/cart.tsx
+++ FoodOrdering/src/app/cart.tsx
@@ -2,18 +2,24 @@ import { View, Text, FlatList, Platform} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useCart } from '@/src/providers/CartProviders';
 import CartListItem from '@/src/components/CartListItem';
+import Button from '@/src/components/Button';
 
 
 const CartScreen = () => {
-    const { items } = useCart();
+    const { items, total } = useCart();
 
     return (
-        <View>
+        <View style={{ padding: 10 }}>
             <FlatList 
             data={items} 
             renderItem={({ item }) => <CartListItem cartItem={item} />} 
-            contentContainerStyle={{ padding: 10, gap: 10 }}
+            contentContainerStyle={{ gap: 10 }}
             />
+
+            <Text style={{ margin: 20, fontSize: 20, fontWeight: '500' }}>
+                Total : ${total}
+            </Text>
+            <Button text='Checkout' />
 
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
