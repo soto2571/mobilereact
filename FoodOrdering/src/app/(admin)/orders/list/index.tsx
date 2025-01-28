5fc995ac1e176b6502b4e18701d682d55b7b5a6a
Sebastian <sebastian.soto1649@gmail.com>
2025-01-28 19:17:38 -0400

    adding subscriptions to listen events

--- FoodOrdering/src/app/(admin)/orders/list/index.tsx
+++ FoodOrdering/src/app/(admin)/orders/list/index.tsx
@@ -1,10 +1,34 @@
 import { Text, FlatList, ActivityIndicator } from 'react-native';
 import OrderListItem from '@/src/components/OrderListItem';
 import { useAdminOrderList } from '@/src/api/orders';
+import { supabase } from '@/src/lib/supabase';
+import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function OrdersScreen() {
 
-    const { data: orders, error, isLoading } = useAdminOrderList({ archive: false });
+    const { 
+        data: orders, 
+        error, 
+        isLoading 
+    } = useAdminOrderList({ archive: false });
+
+    const queryClient = useQueryClient();
+
+    useEffect(() => {
+        const orders = supabase
+        
+    const channels = supabase.channel('custom-insert-channel')
+    .on(
+    'postgres_changes',
+    { event: 'INSERT', schema: 'public', table: 'orders' },
+    (payload) => {
+        console.log('Change received!', payload);
+        queryClient.invalidateQueries(['orders']);
+    }
+    )
+    .subscribe();
+}, []);
 
     if (isLoading) {
         return <ActivityIndicator />;
